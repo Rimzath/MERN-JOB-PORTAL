c908cef34b0f5536843f90c0d99ac4ec1155fd3f
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-09 12:58:49 +0530

    created home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import Home from "./pages/Home";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { CssBaseline } from "@mui/material";
 
-function App() {
-  return <div>Hello Rimzath!</div>;
-}
+const App = () => {
+  return (
+    <>
+      <ToastContainer />
+      <CssBaseline />
+      <ProSidebarProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search/location/:location" element={<Home />} />
+            <Route path="/search/:keyword" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ProSidebarProvider>
+    </>
+  );
+};
 
 export default App;
