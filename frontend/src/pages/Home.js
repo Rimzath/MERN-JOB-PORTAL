cb4b20ade6e8cc98e5f5fb1116115440fa05efd5
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-10 14:11:20 +0530

    created footer

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../component/Navbar";
 import Header from "../component/Header";
+import Footer from "../component/Footer";
 import { Box } from "@mui/material";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Header />
       <div>Welcome to Home</div>
+      <Footer />
     </Box>
   );
 };
