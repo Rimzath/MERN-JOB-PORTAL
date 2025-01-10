ec5448d83396318709ac9888aee45d99067e65f7
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-10 19:24:59 +0530

    created register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -16,6 +17,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/search/location/:location" element={<Home />} />
             <Route path="/search/:keyword" element={<Home />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </BrowserRouter>
       </ProSidebarProvider>
