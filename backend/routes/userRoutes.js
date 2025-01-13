7659d338ed644cf10262efdd6226d360871afad9
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-13 13:39:43 +0530

    install jsonwebtoken and bcryptjs dependncies

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { allUsers, singleUser } = require("../controllers/userController");
+const { isAuthenticated, isAdmin } = require("../middleware/auth");
+
+//user routes
+
+// /api/allusers
+router.get("/allusers", isAuthenticated, isAdmin, allUsers);
+// /api/user/id
+router.get("/user/:id", isAuthenticated, singleUser);
