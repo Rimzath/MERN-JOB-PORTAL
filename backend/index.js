2540d2a6dd74c10b1610800fb3c9790b80cd1a47
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-16 11:35:38 +0530

    deleted created file and reset the backend to connect mongodb again

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-// Import routes
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
-
 const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
 
 // MongoDB connection
 const uri =
@@ -25,10 +17,6 @@ mongoose
     console.error("MongoDB connection error:", err);
     process.exit(1);
   });
-
-// Routes middleware
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
 
 // Define a simple route
 app.get("/", (req, res) => {
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { protect } = require("../middleware/auth");
-const router = express.Router();
-
-router.get("/profile", protect, (req, res) => {
-  res.json(req.user);
-});
-
-module.exports = router;
