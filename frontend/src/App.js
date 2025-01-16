c1155c911b56818afb2cf53fea9ab322e49363ea
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-16 19:43:08 +0530

    added userdashboard to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,15 @@ import "./App.css";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import LogIn from "./pages/LogIn";
+import UserDashboard from "./pages/user/UserDashboard";
+import Layout from "./pages/global/Layout";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { CssBaseline } from "@mui/material";
+
+//HOC
+const UserDashboardHOC = Layout(UserDashboard);
 
 const App = () => {
   return (
@@ -20,6 +25,7 @@ const App = () => {
             <Route path="/search/:keyword" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<LogIn />} />
+            <Route path="/user/dashboard" element={<UserDashboardHOC />} />
           </Routes>
         </BrowserRouter>
       </ProSidebarProvider>
