c5e7384dc944244fdd6eb9e1d61fcb138b372e4b
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-17 20:45:50 +0530

    load jobs type

--- frontend/src/redux/actions/jobTypeAction.js
+++ frontend/src/redux/actions/jobTypeAction.js
@@ -4,7 +4,27 @@ import {
   CREATE_JOB_TYPE_FAIL,
   CREATE_JOB_TYPE_REQUEST,
   CREATE_JOB_TYPE_SUCCESS,
+  JOB_TYPE_LOAD_FAIL,
+  JOB_TYPE_LOAD_REQUEST,
+  JOB_TYPE_LOAD_SUCCESS,
 } from "../constants/jobTypeConstant";
+
+// load jobs type
+export const jobTypeLoadAction = () => async (dispatch) => {
+  dispatch({ type: JOB_TYPE_LOAD_REQUEST });
+  try {
+    const { data } = await axios.get("/api/type/jobs");
+    dispatch({
+      type: JOB_TYPE_LOAD_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: JOB_TYPE_LOAD_FAIL,
+      payload: error.response.data.error,
+    });
+  }
+};
 
 // create jobs category
 export const createJobTypeAction = (jobtype) => async (dispatch) => {
